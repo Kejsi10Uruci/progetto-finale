83cb08c267d76eaa549197e44dbaf4bd131a9e28
Kejsi10Uruci <kejsiuruci48@gmail.com>
2025-05-28 19:32:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True         # INTENT PRIVILEGIATO
+intents.presences = True       # INTENT PRIVILEGIATO
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Abbiamo fatto l\'accesso come {bot.user}')
+
+@bot.command()
+async def trasporti(ctx):
+    await ctx.send("Scegli trasporti sostenibili🌿: riduci l'uso dell'auto privata optando per mezzi pubblici, bicicletta o camminate. Se possibile, considera l'acquisto di un veicolo elettrico. Il settore dei trasporti è responsabile di circa il 30% delle emissioni di gas serra.")
+
+@bot.command()
+async def ridurreriusarericiclare(ctx):
+    await ctx.send("Riduci, riusa e ricicla♻️: limita l'uso di plastica monouso, riutilizza gli oggetti quando possibile e segui le regole della raccolta differenziata per ridurre i rifiuti.")
+
+@bot.command()
+async def cibo(ctx):
+    await ctx.send("Riduci lo spreco alimentare🍽️: compra solo ciò che ti serve, conserva bene il cibo e consuma gli avanzi. Lo spreco alimentare contribuisce alle emissioni di metano.")
+
+@bot.command()
+async def acqua(ctx):
+    await ctx.send("Conserva l'acqua💧: chiudi il rubinetto mentre ti lavi i denti, utilizza lavatrici e lavastoviglie solo a pieno carico e considera l'installazione di dispositivi per il risparmio idrico.")
+
+@bot.command()
+async def casa(ctx):
+    await ctx.send("- Risparmia energia in casa💡: adotta lampadine a LED, spegni gli apparecchi elettronici quando non li usi e regola la temperatura del riscaldamento e del condizionatore per evitare sprechi energetici.")
+    await ctx.send("- Migliora l'efficienza energetica della tua abitazione🏠: isola termicamente la casa, installa pannelli solari e utilizza elettrodomestici a basso consumo energetico per ridurre l'impatto ambientale e risparmiare sulle bollette.")
+
+@bot.command()
+async def consumo(ctx):
+    await ctx.send("Fai scelte di consumo consapevoli🛒: acquista prodotti locali e di stagione, preferisci aziende che adottano pratiche sostenibili e riduci il consumo di carne, poiché l'allevamento intensivo contribuisce significativamente alle emissioni di gas serra.")
+
+@bot.command()
+async def sprecoalimenti(ctx):
+    await ctx.send("Riduci lo spreco alimentare🍽️: compra solo ciò che ti serve, conserva bene il cibo e consuma gli avanzi. Lo spreco alimentare contribuisce alle emissioni di metano ")
+
+bot.run("MTM3NTQ2NjI2OTAzMzIzNDU5NA.Gxn9du.dAlYInXFHfTj2NbnLd6jBxEJl2JjS26CugUKCg")
